b7125a364fc8ae40e91c542bcc572ac2b0c5d2aa
Eduardo Hilário Araujo <eduardohilario.eha@gmail.com>
2024-08-26 21:04:58 -0300

    first commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.programacao.jogo {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.programacao.jogo to javafx.fxml;
+    exports com.programacao.jogo;
+}
